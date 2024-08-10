df93297750fd3807af27f72a05ad76bac51b02cc
Todd Zmijewski <todd@rtc.eco>
2024-08-10 11:53:28 -0400

    Add app build

---
+++ json_plugins_json.bundle.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkcbdc_carbon_reporting_dashboard"] = self["webpackChunkcbdc_carbon_reporting_dashboard"] || []).push([["json_plugins_json"],{
+
+/***/ "./node_modules/@amcharts/amcharts5/plugins/json.js":
+/*!**********************************************************!*\
+  !*** ./node_modules/@amcharts/amcharts5/plugins/json.js ***!
+  \**********************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   JsonParser: () => (/* reexport safe */ _internal_plugins_json_Json__WEBPACK_IMPORTED_MODULE_0__.JsonParser),\n/* harmony export */   Serializer: () => (/* reexport safe */ _internal_plugins_json_Serializer__WEBPACK_IMPORTED_MODULE_1__.Serializer)\n/* harmony export */ });\n/* harmony import */ var _internal_plugins_json_Json__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../.internal/plugins/json/Json */ \"./node_modules/@amcharts/amcharts5/.internal/plugins/json/Json.js\");\n/* harmony import */ var _internal_plugins_json_Serializer__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../.internal/plugins/json/Serializer */ \"./node_modules/@amcharts/amcharts5/.internal/plugins/json/Serializer.js\");\n\r\n\r\n//# sourceMappingURL=json.js.map\n\n//# sourceURL=webpack://cbdc-carbon-reporting-dashboard/./node_modules/@amcharts/amcharts5/plugins/json.js?");
+
+/***/ })
+
+}]);
